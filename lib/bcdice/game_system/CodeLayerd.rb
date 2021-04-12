8d47c3c4bf594533379516aa3a3fd8f2da5911ce
GenKuzumochi <genkuzumochi@gmail.com>
2021-04-13 00:58:38 +0900

    微調整

--- lib/bcdice/game_system/CodeLayerd.rb
+++ lib/bcdice/game_system/CodeLayerd.rb
@@ -30,7 +30,7 @@ module BCDice
           デフォルトダイス：10面
       MESSAGETEXT
 
-      register_prefix('[+-]?\d*CL')
+      register_prefix('[+-]?\d*CL([+-]\d+)?[@\d]*')
 
       def initialize(command)
         super(command)
@@ -41,61 +41,61 @@ module BCDice
       def eval_game_system_specific_command(command)
         debug('eval_game_system_specific_command command', command)
 
-        m = /([+-]?\d+)?CL([+-]\d+)?(@(\d))?(\[(\d+)\])?([+-]\d+)?(>=(\d+))?/i.match(command)
-        return nil unless m
+        result = ''
 
-        base = (m[1] || 1).to_i
-        modifier1 = m[2].to_i
-        target = (m[4] || 6).to_i
-        critical_target = (m[6] || 1).to_i
-        modifier2 = m[7].to_i
-        diff = m[9].to_i if m[9]
-        check_roll(command, base, target, critical_target, diff, modifier1 + modifier2)
+        case command
+        when /([+-]?\d+)?CL([+-]\d+)?(@(\d))?(\[(\d+)\])?([+-]\d+)?(>=(\d+))?/i
+          m = Regexp.last_match
+          base = (m[1] || 1).to_i
+          modifier1 = m[2].to_i
+          target = (m[4] || 6).to_i
+          critical_target = (m[6] || 1).to_i
+          modifier2 = m[7].to_i
+          diff = m[9].to_i
+          result = check_roll(base, target, critical_target, diff, modifier1 + modifier2)
+        end
+
+        return nil if result.empty?
+
+        return "#{command} ＞ #{result}"
       end
 
-      def check_roll(command, base, target, critical_target, diff, modifier)
+      def check_roll(base, target, critical_target, diff, modifier)
         if base <= 0 # クリティカルしない1D
           critical_target = 0
           base = 1
         end
-        result = Result.new
 
         target = 10 if target > 10
         dice_list = @randomizer.roll_barabara(base, 10).sort
         success_count = dice_list.count { |x| x <= target }
         critical_count = dice_list.count { |x| x <= critical_target }
-        result.critical = critical_count > 0
         success_total = success_count + critical_count + modifier
 
         mod_text = Format.modifier(modifier)
 
         # (10d10+5)
-        text = "#{command} ＞ (#{base}d10#{mod_text}) ＞ [#{dice_list.join(',')}]#{mod_text} ＞ "
-        text += "判定値[#{target}] " unless target == 6
-        text += "クリティカル値[#{critical_target}] " unless critical_target == 1
-        text += "達成値[#{success_count}]"
+        result = "(#{base}d10#{mod_text}) ＞ [#{dice_list.join(',')}]#{mod_text} ＞ "
+        result += "判定値[#{target}] " unless target == 6
+        result += "クリティカル値[#{critical_target}] " unless critical_target == 1
+        result += "達成値[#{success_count}]"
 
-        if success_count <= 0
-          result.fumble = true
-          result.failure = true
-          result.text = "#{text} ＞ ファンブル！"
-          return result
-        end
+        return "#{result} ＞ ファンブル！" if success_count <= 0
 
-        text += "+クリティカル[#{critical_count}]" if result.critical?
-        text += mod_text
-        text += "=[#{success_total}]" if result.critical? || modifier != 0
+        result += "+クリティカル[#{critical_count}]" if critical_count > 0
+        result += mod_text
+        result += "=[#{success_total}]" if critical_count > 0 || modifier != 0
 
-        if diff.nil?
-          result.text = "#{text} ＞ #{success_total}"
-        elsif success_total >= diff
-          result.text = "#{text} ＞ 成功"
-          result.success = true
-        else
-          result.text = "#{text} ＞ 失敗"
-          result.failure = true
-        end
-        return result
+        success_text =
+          if diff == 0
+            success_total.to_s
+          elsif success_total >= diff
+            "成功"
+          else
+            "失敗"
+          end
+
+        return "#{result} ＞ #{success_text}"
       end
     end
   end
